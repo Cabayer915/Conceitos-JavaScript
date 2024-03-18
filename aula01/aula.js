8233ce4d3979bedebc0b233cd73870dd46a5cc4c
Cabayer915 <cauaribeirob915@gmail.com>
2024-03-18 11:54:59 -0300

    Adicionando Atividades

---
+++ aula01/aula.js
@@ -0,0 +1,15 @@
+// Let atribui um valor que pode se variar
+let variavelLet = 10;
+
+console.log(variavelLet)
+
+variavelLet = 20;
+
+console.log(variavelLet)
+
+// Const passa um valor Constante que não pode se alterar
+const variavelConst = 3.14;
+
+console.log(variavelConst)
+
+console.log('Printando Algo')
---
+++ aula02/aula.js
@@ -0,0 +1,13 @@
+// O JavaScript trabalha com operações lógicas e simples operadores da matemática
+
+let soma = 10 + 10;
+console.log(soma)
+
+let subtracao = 10 - 10;
+console.log(subtracao)
+
+let multiplicacao = 10 * 10;
+console.log(multiplicacao)
+
+let divisao = 10/2;
+console.log(divisao)
